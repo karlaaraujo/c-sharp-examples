193af88c79a3457ec55b846de455bb684e17c754
Karla Araújo <karlachs.a@gmail.com>
2023-04-28 11:28:34 -0300

    Adicionar arquivos de projeto.

---
+++ CSharpExamples/HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpExamples
+{
+    internal class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
