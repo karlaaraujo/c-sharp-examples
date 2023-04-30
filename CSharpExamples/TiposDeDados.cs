f720a7993fbccc05155b1320c671c3b2a52a30e6
Karla Araújo <karlachs.a@gmail.com>
2023-04-30 12:23:20 -0300

    Add TiposDeDados.cs

---
+++ CSharpExamples/TiposDeDados.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace CSharpExamples
+{
+  class TiposDeDados
+  {
+    static void Main(string[] args)
+    {
+      int meuInteiro = 5;               // inteiro 
+      double meuDouble = 5.99D;  // ponto flutuante
+      char minhaLetra = 'A';         // caractere
+      bool meuBool = true;          // booleano
+      string meuTexto = "Ola";     // string
+
+      Console.WriteLine(meuInteiro);
+      Console.WriteLine(meuDouble);
+      Console.WriteLine(minhaLetra);
+      Console.WriteLine(meuBool);
+      Console.WriteLine(meuTexto);
+    }
+  }
+}
