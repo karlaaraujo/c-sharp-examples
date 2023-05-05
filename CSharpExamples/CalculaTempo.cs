a7bb28365b2d3b57e519b449d89db4bb6fad74e5
Ingrid Mônica <118575636+ingridmonica@users.noreply.github.com>
2023-05-05 16:33:37 -0300

    Adicionando arquivo CalculaTempo (#4)
    * Adicionando arquivo CalculaTempo
    
    * Adicionando arquivo CalculaTempo

---
+++ CSharpExamples/CalculaTempo.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace CSharpExemplos
+{
+    class CalculaTempo
+    {
+        static void Main(string[] args)
+        {
+            // lê o tempo em segundos a partir do usuário
+            Console.Write("Informe o tempo em segundos: ");
+            int tempoSegundos = int.Parse(Console.ReadLine());
+
+            // calcula o tempo em horas, minutos e segundos
+            int horas = tempoSegundos / 3600;
+            int minutos = (tempoSegundos % 3600) / 60;
+            int segundos = tempoSegundos % 60;
+
+            // imprime o tempo em horas, minutos e segundos
+            Console.WriteLine("O tempo é o equivalente a: {0} horas, {1} minutos e {2} segundos.", horas, minutos, segundos);
+        }
+    }
+}
